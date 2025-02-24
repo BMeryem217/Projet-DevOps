4e02f0039b30bff34aa5e3a823fa00116ed75cf8
meryem <meryembounit217@gmail.com>
2025-02-24 00:21:03 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 # Données initiales pour l'API REST
 tasks = [
-    {"id": 1, "title": "Faire les courses du ramadan", "done": False},
+    {"id": 1, "title": "Faire les courses du ramadan karem", "done": False},
     {"id": 2, "title": "Apprendre Flask", "done": True},
 ]
 
